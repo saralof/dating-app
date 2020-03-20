ae4b1a53e27c895bed89d5152df77a3587cb2843
saralof <60660486+saralof@users.noreply.github.com>
2020-03-20 10:30:29 +0100

    ejs set-up
    A simple set-up for EJS

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongo = require('mongodb')
 const session = require('express-session')
+const ejs = require('ejs');
+const port = 3000;
 
 //setup dotenv
 require('dotenv').config()
@@ -20,16 +22,19 @@ mongo.MongoClient.connect(url, function (err, client) {
     db = client.db(process.env.DB_NAME)
 })
 
-app
-    .use(session({
+    app.set('view engine', 'ejs')
+    app.set('views', 'views')
+    app.use(session({
     resave: false,
     saveUninitialized: true,
     secret: process.env.SESSION_SECRET,
     cookie: { sameSite: true,}
     }))
-    .listen(process.env.SERVER_PORT, () => {
+    app.listen(process.env.SERVER_PORT, () => {
         console.log('Server is up on port ' + process.env.SERVER_PORT)
     })
     app.get('/', function (req, res) {
-        res.send('hello world')
+        res.render('index')
     })
+    
+    app.listen(port, () => console.log('Example app listening on port ${port}!'));
